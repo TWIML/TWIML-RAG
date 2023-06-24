8d572bf03addc7c651246a1e6802bdc37cfe44d7
dplutcho <darin.plutchok@gmail.com>
2023-06-24 14:03:45 -0700

    Testing deploy of default app which is running fine locally

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 """Python file to serve as the frontend"""
+import os
+
 import streamlit as st
 from streamlit_chat import message
 
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+import openai
+openai.api_key  = os.environ['OPENAI_API_KEY']
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
